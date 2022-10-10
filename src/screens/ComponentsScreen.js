1c70d5551ec5c49844c2e06bbb2ba51919b23286
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-10-10 17:24:59 +0900

    JSX Solution

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
-    return <Text style={styles.textStyle}>Components Screen</Text>;
+    const name = "Toshi";
+
+    return (
+        <View>
+            <Text style={styles.textStyle}>Getting started with ReactNative!</Text>
+            <Text style={styles.subHeaderStyle}>My name is {name}</Text>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
     textStyle: {
-        fontSize: 30
+        fontSize: 45
+    },
+    subHeaderStyle: {
+        fontSize: 20
     }
 });
 

252e2e4ec86f467a9ff24f09b46add5872261fe8
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-10-15 15:51:36 +0900

    Showing Images

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
-const ImageDetail = () => {
-    return <Text>Image Detail</Text>;
+const ImageDetail = props => {
+    // console.log(props);
+    return (
+        <View>
+            <Image source={require('../../assets/beach.jpg')} />
+            <Text>{props.title}</Text>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({});

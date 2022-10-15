7590d346bec9d805ff9affe1916c19ab7f43c1b6
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-10-15 17:09:22 +0900

    State in Action

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
 const CounterScreen = () => {
+  const [counter, setCounter] = useState(0);
+
+  console.log(counter);
+
   return (
     <View>
-      <Text>Current Count: </Text>
+      <Button
+        title="Increase"
+        onPress={() => {
+          // Don't do this!
+          // counter++;
+
+          // Do this!
+          setCounter(counter + 1);
+        }}
+      />
+      <Button
+        title="Decrease"
+        onPress={() => {
+          setCounter(counter - 1);
+        }}
+      />
+      <Text>Current Count: {counter}</Text>
     </View>
   );
 };

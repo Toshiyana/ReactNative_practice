4fd0345c614ca7cf1822770acb887fcfe8ea3b8d
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-10-16 16:35:12 +0900

    Exercise Solution

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,33 +1,70 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
-const CounterScreen = () => {
-  const [counter, setCounter] = useState(0);
+// state管理にReducerを用いた場合
+const reducer = (state, action) => {
+  // state === { count: number}
+  // action === { type: 'increment' || 'decrement', payload: 1 }
+  switch (action.type) {
+    case "increment":
+      return { ...state, count: state.count + action.payload };
+    case "decrement":
+      return { ...state, count: state.count - action.payload };
+    default:
+      return state;
+  }
+};
 
-  // console.log(counter);
+const CounterScreen = () => {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
 
   return (
     <View>
       <Button
         title="Increase"
         onPress={() => {
-          // Don't do this!
-          // counter++;
-
-          // Do this!
-          setCounter(counter + 1);
+          dispatch({ type: "increment", payload: 1 });
         }}
       />
       <Button
         title="Decrease"
         onPress={() => {
-          setCounter(counter - 1);
+          dispatch({ type: "decrement", payload: 1 });
         }}
       />
-      <Text>Current Count: {counter}</Text>
+      <Text>Current Count: {state.count}</Text>
     </View>
   );
 };
+
+// state管理にuseStateを用いた場合
+// const CounterScreen = () => {
+//   const [counter, setCounter] = useState(0);
+
+//   // console.log(counter);
+
+//   return (
+//     <View>
+//       <Button
+//         title="Increase"
+//         onPress={() => {
+//           // Don't do this!
+//           // counter++;
+
+//           // Do this!
+//           setCounter(counter + 1);
+//         }}
+//       />
+//       <Button
+//         title="Decrease"
+//         onPress={() => {
+//           setCounter(counter - 1);
+//         }}
+//       />
+//       <Text>Current Count: {counter}</Text>
+//     </View>
+//   );
+// };
 
 const styles = StyleSheet.create({});
 

c48c0a73c9e0e32293bc3a1b1b2d1fdcbc1fc8e6
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-10-16 15:06:46 +0900

    Restoring Validation

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -10,17 +10,24 @@ const reducer = (state, action) => {
 
   switch (action.colorToChange) {
     case "red":
-      // Don't do this!
+      // Don't do this! (Don't change state directly!)
       // state.red = state.red - 15;
 
       // Do this!
-      return { ...state, red: state.red + action.amount };
+      return state.red + action.amount > 255 || state.red + action.amount < 0
+        ? state
+        : { ...state, red: state.red + action.amount };
     // same below
     // return { green: 0, blue: 0, red: state.red + action.amount };
     case "green":
-      return { ...state, green: state.green + action.amount };
+      return state.green + action.amount > 255 ||
+        state.green + action.amount < 0
+        ? state
+        : { ...state, green: state.green + action.amount };
     case "blue":
-      return { ...state, blue: state.blue + action.amount };
+      return state.blue + action.amount > 255 || state.blue + action.amount < 0
+        ? state
+        : { ...state, blue: state.blue + action.amount };
     default:
       return state;
   }
